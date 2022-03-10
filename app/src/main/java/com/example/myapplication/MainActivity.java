b5ebdafd55e6f9ce9c58553162ad830410bca09b
severi <severi.salonen.ss@gmail.com>
2022-03-10 15:19:40 +0200

    L071-3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    EditText inputText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        inputText = (EditText) findViewById(R.id.editText);
     }
 
     public void function(View v) {
         System.out.println("Hello world!");
+        text.setText("Hello World!");
+    }
+
+    public void textFunction(View v) {
+        text.setText(inputText.getText());
+
     }
 }
